59aec62569c2e93a7673eecd46a0a1f5e8f8217d
sooyeon9 <sooyean9@naver.com>
2022-04-20 17:01:02 +0900

    Entity 주의사항 수정

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -18,14 +18,21 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne // 하나의 회원으로부터 여러개의 order을 받을 수 있으므로
+    @ManyToOne(fetch = FetchType.LAZY) // 하나의 회원으로부터 여러개의 order을 받을 수 있으므로
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
+    /* CascadeType.ALL
+    persist(orderItemA)
+    persist(orderItemB)
+    persist(orderItemC)
+    persist(order)
+    --> persist(order) 원래 각자 해주어야하는데 요렇게 한줄로 대체 가능 (저장, 삭제할 때)
+    */
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -33,4 +40,20 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문상태 [ORDER, CANCEL]
+
+    //==연관관계 메서드==//
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItems(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }

d6e47c5f41c97bedea1c9512c60a97b3ad3f1bb0
sooyeon9 <sooyean9@naver.com>
2022-04-21 18:20:36 +0900

    ItemController & BookForm

---
+++ src/main/java/jpabook/jpashop/controller/BookForm.java
@@ -0,0 +1,18 @@
+package jpabook.jpashop.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class BookForm {
+
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    // Book 고유 속성
+    private String author;
+    private String isbn;
+}
---
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -0,0 +1,54 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.item.Address;
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "/items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(@Valid BookForm form) {
+        Book book = new Book();
+        // 지금은 이렇게 setter를 쓰지만 생성 시에 모두 바인딩되게끔 하고 set 함수를 줄이는게 나은 설계이다
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/items";
+    }
+
+    /**
+     * 상품 목록
+     */
+    @GetMapping(value = "/items")
+    public String list(Model model) {
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
+    }
+}
